33950125d2f3814f42b371acdb7082807c8625a3
Gustavo Gantois <gugagantois@gmail.com>
2024-12-09 08:27:03 -0300

    feat:Creating file PrimitiveTypes.java

---
+++ src/je04_reserve_words/PrimitiveTypes.java
@@ -0,0 +1,11 @@
+package je04_reserve_words;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        //declaring variables:
+        int number = 123;
+        double decimal = 123.0;
+        byte b = 127;
+        char caracter = 'a';
+    }
+}
