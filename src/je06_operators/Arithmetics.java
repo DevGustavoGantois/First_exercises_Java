f99ce3f040cc6866f1b6ab2dab1a0bd8d3de015d
Gustavo Gantois <gugagantois@gmail.com>
2024-12-11 10:52:49 -0300

    feat:Adding concat numbers and strings.

---
+++ src/je06_operators/Arithmetics.java
@@ -0,0 +1,31 @@
+package je06_operators;
+
+public class Arithmetics {
+    double sum = 10.5 + 15.7;
+    int subtraction = 113 - 25;
+    int multiplication = 20 * 7;
+    int division = 15 / 3;
+    int module = 18 % 3;
+    double result = (10 * 7) + (20/4);
+
+    //Declarando
+    int n1 = 10;
+    int n2 = 5;
+
+    //Forma literal
+    n2 = n2 + n1;
+
+    //forma abreviada
+    n2 += n1;
+
+    String completeName = "JAVA" + "LANGUAGE";
+    String concat = "?";
+    concat = 1 + 1 + 1 + "1";
+
+    concat = 1 + "1" + 1 + 1;
+
+    concat = 1 + "1" + 1 + "1";
+
+    concat = "1" + (1 + 1 + 1);
+
+}
