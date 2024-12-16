fe5f328dabe837ce9e60e56879bfb1737b160834
Gustavo Gantois <gugagantois@gmail.com>
2024-12-15 21:25:38 -0300

    feat:Create class JavaDoc and Calculator for example with comments.

---
+++ src/je08_documentation/Calculator.java
@@ -0,0 +1,13 @@
+package je08_documentation;
+/*
+ * Simple Calculator
+ * @author Gustavo Gantois
+ *@since v1.0
+ * */
+
+public class Calculator {
+    Integer dividir(Integer dividendo, Integer divisor ) {
+        return dividendo / divisor;
+
+    }
+}
---
+++ src/je08_documentation/JavaDoc.java
@@ -0,0 +1,10 @@
+package je08_documentation;
+
+public class JavaDoc {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        Integer result = calculator.dividir(10, 5);
+        String name = "";
+    }
+}
